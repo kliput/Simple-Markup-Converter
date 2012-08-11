b4384af1c11e111e10f46065fa3b096dbd25fde9
Jakub Liput <jakub.liput@gmail.com>
2012-08-11 23:34:52 +0200

    Rozbudowa programu głównego; translator-manekin - przekazuje wejście tekstowe na wyjście

--- main.py
+++ main.py
@@ -17,25 +17,83 @@
 # You should have received a copy of the GNU General Public License
 # along with SimpleMarkupConverter.  If not, see <http://www.gnu.org/licenses/>.
 
-from translator.txt2tags import Txt2Tags
-import ply.lex as lex
+from translator.txt2tags import Txt2TagsInput, Txt2TagsOutput
+from translator.dummy import PassTranslator
+from ply import lex, yacc
 import sys
 
+# stałe kierunku translacji
+IN = "input"
+OUT = "output"
+
+# przechowuje odwzorowanie kodu_<input/output> -> klasa translatora
+translator_map = {
+#                  "txt2tags":
+#                  {IN: Txt2TagsInput, OUT: Txt2TagsInput },
+                  "dummy":
+                  {IN: PassTranslator, OUT: PassTranslator }
+                }
 
 if __name__ == '__main__':
+        # TODO: parametry wywołania - parsing:
+        # ./smc [-o output_file] --iformat=code_input --oformat=code_output input_file
+        # -o plik wyjściowy (opcjonalne, może wypisać na stdout)
+        # -iformat kod_formatu_wejściowego
+        # -oformat kod_formaty_wyjściowego
+        # kody: txt2tags, textile, dokuwiki, html (tylko wyjściowe)
+        
+        # otworzenie pliku z parametru
         try:
             f = open(sys.argv[1], "r")
-        except:
-            print("File open error")
+        except Exception as e:
+            print("File open error: %s" % str(e))
             exit(1)
         
-        text = f.read()
-        
-        input_parser = Txt2Tags()
-        
+        # odczyt pliku
         try:
-            output = input_parser.run(text)
+            text = f.read()
+        except Exception as e:
+            print("File read error: %s" % str(e))
+            exit(1)
+        
+        # TODO: wczytanie z linii komend
+        # wybór odpowiednich translatorów wejściowych/wyjściowych
+        file_format = {}
+        file_format[IN] = "dummy"
+        file_format[OUT] = "dummy"
+        
+        translator = {}
+        
+        # TODO: można obsłużyć gdy jest translator, ale w nie w tą stronę
+        try:
+            # wyszukanie w mapie odpowiednich translatorów we/wy i ich konstrukcja
+            for direction in [IN, OUT]:
+                translator_type = translator_map[file_format[direction]][direction]
+                translator[direction] = translator_type()
+        except KeyError:
+            print("Wrong %s format specified: %s" % (direction, file_format[direction]))
+            exit(2)
+        except SyntaxError as e:
+            # błąd w konstrukcji translatora
+            print("Construction of %s parser %s failed: %s" % (direction, translator_type.__name__, e))
+            exit(4)
+        except yacc.LALRError as e:
+            print("Construction of %s parser %s failed: %s" % (direction, translator_type.__name__, e))
+            exit(4)
+            
+        try:
+            # wywoływanie kolejnych translatorów
+            for direction in [IN, OUT]:
+                text = translator[direction].run(text)
+            # TODO: możliwy tryb wyjścia do pliku
+            print(text)
+            exit(0)
         except lex.LexError as e:
-            print("Parsing error:", e)
-        else:
-            print(output);
+            print("Translation %s lexer error: %s" % (direction, e))
+        except Exception as e:
+            print("Error: %s" % str(e))
+        
+        # zakończono niepowodzeniem
+        exit(3)
+        
+        
--- translator/dummy.py
+++ translator/dummy.py
@@ -3,19 +3,17 @@
 from .translator import Translator
 import logging
 
-#import re
-
-class Txt2TagsOutput(Translator):
-    pass
-
-class Txt2TagsInput(Translator):
-
+class PassTranslator(Translator):
+    '''
+    Pusty translator, który zwraca ten sam tekst, który otrzymał.
+    '''
+    
     log = logging.getLogger(__name__)
 
     output = ''
 
     def __init__(self):
-        self.log.debug('Txt2Tags constructor')
+        self.log.debug('PassTranslator constructor')
         super().__init__()
     
 
@@ -23,10 +21,8 @@ class Txt2TagsInput(Translator):
         'PLAIN_TEXT',
     )
     
-#    literals = ['\n']
-
     t_PLAIN_TEXT = r'(.|\n)+'
-
+    
 
     def p_text(self, p):
         '''
@@ -34,4 +30,3 @@ class Txt2TagsInput(Translator):
         '''
         p[0] = p[1]
         self.output = p[0]
-
