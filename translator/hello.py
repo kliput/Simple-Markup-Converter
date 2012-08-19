37347f473da0610e6121aac429f87506ca50919a
Jakub Liput <jakub.liput@gmail.com>
2012-08-19 12:57:27 +0200

    Rozwój testowego parsera Hello

--- translator/hello.py
+++ translator/hello.py
@@ -11,33 +11,72 @@ class HelloTranslator(Translator):
         
     
     tokens = (
-        'PLAIN',
-        'BOLD',
+        'WORD',
     )
     
-    t_PLAIN = r'(.|\n)+'
+    # ciąg zwartych znaków
+    t_WORD = r'[\w\~\`\!\@\#\$\%\^\&\*\(\)\-\+\[\]\{\}\:\;\"\'\<\>\?\,\.\/\|\\]+'
+
+    t_ignore = ' \t\n'
     
-    t_BOLD = r'\*\*'
-    
-    def p_text(self, p):
+#    def t_NL_ONE(self, t):
+#        r'\n'
+#        self.log.debug("newline one")
+#    
+#    def t_NL_TWO(self, t):
+#        r'\n\n'
+#        self.log.debug("newline two")
+
+    def p_document(self, p):
         '''
-        text    :    text PLAIN
-                |    text bold
+        document    : document paragraph
         '''
-        self.log.debug('p_text')
+        self.log.debug('document    : document paragraph')
         p[0] = p[1] + p[2]
-    
-    def p_bold(self, p):
+        
+    def p_document_blank(self, p):
         '''
-        bold    :    BOLD text BOLD
+        document    : 
         '''
-        self.log.debug('p_bold')
-        p[0] = '<b>%s</b>' % p[2]
-    
-    def p_text_blank(self, p):
-        '''
-        text    : 
-        '''
-        self.log.debug('p_text_blank')
+        self.log.debug("document blank")
         p[0] = ''
         
+    # akapit zawierający więcej elementów
+    def p_paragraph(self, p):
+        '''
+        paragraph   : paragraph element '\\n' '\\n'
+        '''
+        self.log.debug('paragraph paragraph element') 
+        p[0] = p[0] + p[1]
+        
+    # ostatni bądź jedyny element w akapicie
+    def p_paragraph_last_element(self, p):
+        '''
+        paragraph    : element
+        '''
+        self.log.debug('paragraph element')
+        p[0] = p[1]
+        
+    ## TODO element
+    def p_element(self, p):
+        '''
+        element    : plain 
+        '''
+        self.log.debug('element plain')
+        p[0] = p[1]
+        
+    def p_plain(self, p):
+        '''
+        plain    : plain WORD
+        '''
+        self.log.debug('plain WORD: %s' % p[2])
+        # między dwoma ciągami znaków jest zawsze pojedyncza spacja
+        p[0] = p[1] + ' ' + p[2]
+        
+    def p_plain_last(self, p):
+        '''
+        plain    : WORD
+        '''
+        self.log.debug('plain last word: %s' % p[1])
+        p[0] = p[1]
+    
