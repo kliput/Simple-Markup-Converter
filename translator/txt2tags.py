006da62d3b3e96b630dcf3cb8a0b1292466e8369
Jakub Liput <jakub.liput@gmail.com>
2012-09-02 22:56:59 +0200

    Txt2Tags przed wprowadzaniem nowych pomysłów.

--- translator/txt2tags.py
+++ translator/txt2tags.py
@@ -11,21 +11,57 @@ class Txt2TagsToXML(Translator):
         
     
     tokens = (
-        'WORD',
         'PAREND',
         'NEWLINE',
+#        'BOLD_START',
+#        'BOLD_END',
+        'WORD',
+        'BOLD',
     )
     
+    t_ignore = ' \t'
+    
+#    char_set = r'\w\~\`\!\@\#\$\%\^\&\*\(\)\-\+\[\]\{\}\:\;\"\'\<\>\?\,\.\/\|\\'
+    
     # ciąg zwartych znaków
-    t_WORD = r'[\w\~\`\!\@\#\$\%\^\&\*\(\)\-\+\[\]\{\}\:\;\"\'\<\>\?\,\.\/\|\\]+\n??'
+    t_WORD = r'[^\s]+'
+    
+    def t_BOLD_START(self, t):
+        r'\*\*(?=[^\s])'
+        self.log.debug('BOLD START token: ' + t.value)
+        return t
 
-    # 1 znak nowej linii - ignorowany przy końcowym formatowaniu
+    def t_BOLD_END(self, t):
+        r'(?<=[^\s])\*\*'
+#        r'\**\*\*'
+        self.log.debug('BOLD END token: ' + t.value)
+        return t
+    
+    # TODO jest to pewne rozwiązanie, ale w kontekście całości - źle
+#    def t_BOLD(self, t):
+#        r'\*\*([^\s](|.*?[^\s])\**)\*\*'
+#        self.log.debug('BOLD TOKEN: ' + t.value)
+#        t.value = '<b>%s</b>' % t.value
+#        return t
+    
+#    def t_WORD(self, t):
+#        r'[^\s]+(?<!\*)'
+#        self.log.debug('WORD token: ' + t.value)
+#        return t
+
+    # 1 znak nowej linii - może pojawić się w ramach akapitu
     t_NEWLINE = '\\n'
 
     # 2 lub więcej znaków nowej linii - oddziela akapit
     t_PAREND = '\\n{2,}'
-
-    t_ignore = ' \t'
+    
+#    t_BOLD_START = r'\*\*(?=[^\s])' 
+#    t_BOLD_END = r'(?<=[^\s])\*\*'
+    
+    
+#    precedence = (
+#        ('right', 'BOLD_END'),
+#    )
     
     # dokument z wieloma akapitami
     def p_document_multi(self, p):
@@ -33,7 +69,6 @@ class Txt2TagsToXML(Translator):
         document    : document PAREND paragraph
         '''
         self.log.debug('document: document (%s)  PAREND paragraph (%s)' % (p[1], p[3]))
-#        p[0] = '%s <p>%s</p>' % (p[1], p[3])
         p[0] = p[1] + ' ' + p[3]
     
     # dokument z jednym akapitem, bądź pierwszy akapit
@@ -42,7 +77,6 @@ class Txt2TagsToXML(Translator):
         document    : paragraph
         '''
         self.log.debug('document: paragraph (%s)' % (p[1]))
-#        p[0] = '<p>%s</p>' % p[1]
         p[0] = p[1]
     
 #    # dokument może być w ogóle pusty
@@ -82,26 +116,33 @@ class Txt2TagsToXML(Translator):
     # akapit zawierający więcej elementów przedzielone pojedynczym znakiem nowej linii
     def p_paragraph_content_wnl(self, p):
         '''
-        paragraph_content   : paragraph_content NEWLINE element
+        paragraph_content   : paragraph_content element NEWLINE
         '''
-        self.log.debug('paragraph_content: paragraph_content (%s) NEWLINE element (%s)' % (p[1], p[3])) 
-        p[0] = p[1] + '\n' + p[3]
-        
-    # ostatni bądź jedyny element w akapicie
-    def p_paragraph_content_last_element(self, p):
+        self.log.debug('paragraph_content: paragraph_content (%s) element (%s) NEWLINE' % (p[1], p[2])) 
+        p[0] = p[1] + p[2] + '\n'
+
+    # TODO co to jest?
+    # akapit zawierający więcej elementów przedzielone pojedynczym znakiem nowej linii
+    def p_paragraph_content_blank(self, p):
         '''
-        paragraph_content    : element
+        paragraph_content   : 
         '''
-        self.log.debug('paragraph_content: element (%s)' % (p[1]))
-        p[0] = p[1]    
+        p[0] = ''
         
     ## TODO element
     def p_element(self, p):
         '''
         element    : plain 
+                    | BOLD
         '''
-        self.log.debug('element: plain (%s)' % (p[1]))
+        self.log.debug('element: (...) (%s)' % (p[1]))
         p[0] = p[1]
+        
+#    def p_bold(self, p):
+#        '''
+#        bold    : BOLD_START plain BOLD_END
+#        '''
+#        p[0] = '<b>%s</b>' % p[2]
         
     def p_plain(self, p):
         '''
